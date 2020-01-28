2db2963d004e2e31bd9ddf7b8fc840ea1f0142cf
SashaGlim2 <60271883+SashaGlim2@users.noreply.github.com>
2020-01-28 10:36:45 -0800

    Update partners_in_crime.py

--- partners_in_crime.py
+++ partners_in_crime.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'parters in crime' # Only 10 chars displayed
+team_name = 'team14' # Only 10 chars displayed
 strategy_name = 'Strategy'
 strategy_description = 'A strategy we made'
     
